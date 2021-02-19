2522e3fde10cc5bed021f0e30558272b7f473c33
tomashphill <tomashphill@gmail.com>
2021-02-19 03:38:17 -0500

    Added socket functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,69 +1,83 @@
 from flask import Flask, render_template, jsonify
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+from collections import defaultdict
 from pathlib import Path
+import random
 import sqlite3
-import json
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'super_secret_000'
 socketio = SocketIO(app)
 
 ROOT = Path(app.root_path)
 DB = ROOT / 'model' / 'grid.db'
+MAX_CELLS = 1000
 
 @app.route('/', methods=['GET'])
 def homepage():
     return render_template('index.html')
 
-@app.route('/data', methods=['GET'])
-def show_table():
+@app.route('/get_document', methods=['GET'])
+def get_document():
     with sqlite3.connect(DB) as conn:
         c = conn.cursor()
         c.execute("""SELECT * FROM grid""")
         rows = c.fetchall()
-    
-    rows = [(int(x), int(y), c) for x, y, c in rows]
-
-    maxX, _, _ = max(rows, key=lambda r: r[0])
-    _, maxY, _ = max(rows, key=lambda r: r[1])
-
-    array = [['' for y in range(maxY+1)] for x in range(maxX+1)]
-
-    for x, y, c in rows:
-        array[x][y] = c
-
-    return jsonify(array)
+    doc = [c if c else ' ' for x, y, c in rows]
+    return jsonify(doc)
 
 @socketio.on('initialize')
-def initialize(msg):
-    print(str(msg))
+def initialize(json, methods=['POST']):
+    rand_x = random.randint(0, MAX_CELLS - json['n_cells_w'] - 2)
+    rand_y = random.randint(0, MAX_CELLS - json['n_cells_h'] - 2)
+    rand_x = 0
+    rand_y = 0
+    with sqlite3.connect(DB) as conn:
+        c = conn.cursor()
+        c.execute(f"""
+            SELECT * FROM grid
+            WHERE 
+                X >= {rand_x} AND X <= {rand_x + json['n_cells_w']-1}
+                AND Y >= {rand_y} AND Y <= {rand_y + json['n_cells_h']-1}
+        """)
+        rows = c.fetchall()
+    data = defaultdict(lambda: {})
+    for x, y, c in rows:
+        data[x][y] = c
+    emit('update_grid', data)
 
 @socketio.on('edit_cell')
 def edit_cell(json, methods=['GET', 'POST']):
-    print(str(json))
+    socketio.emit('update_cell', json, include_self=False)
     with sqlite3.connect(DB) as conn:
-        print(str(json))
         c = conn.cursor()
         c.execute(f"""
-            INSERT INTO grid (X, Y, char)
-            VALUES ({ json['X'] }, { json['Y'] }, '{ json['value'] }')
+            UPDATE grid 
+            SET char = '{ json['value'] }'
+            WHERE X = { json['X'] } AND Y = { json['Y'] }
         """)
         conn.commit()
 
-def create_table(conn):
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS grid (
-            X integer,
-            Y integer,
-            char CHARACTER(1) NOT NULL,
-            PRIMARY KEY (X, Y)
-        )
-    """)
-    c.close()
+def create_table():
+    with sqlite3.connect(DB) as conn:
+        c = conn.cursor()
+        c.execute("""
+            CREATE TABLE IF NOT EXISTS grid (
+                X INTEGER,
+                Y INTEGER,
+                char CHARACTER(1) NOT NULL,
+                PRIMARY KEY (X, Y)
+            )
+        """)
+        # for x in range(MAX_CELLS):
+        #     for y in range(MAX_CELLS):
+        #         c.execute(f"""
+        #             INSERT INTO grid (X, Y, char)
+        #             VALUES ({ x }, { y }, ' ')
+        #         """)
+        conn.commit()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
-    conn = sqlite3.connect(DB)
-    create_table(conn)
-
+    # create_table()
+    socketio.run(app)
--- app/static/index.js
+++ app/static/index.js
@@ -1,10 +1,10 @@
 const grid = document.getElementById('grid');
-console.log(io)
+var cells;
 var socket;
 
 (() => {
-    grid.style.height = window.innerHeight + 'px';
-    grid.style.width = window.innerWidth + 'px';
+    grid.style.height = window.innerHeight - 60 + 'px';
+    grid.style.width = window.innerWidth - 40 + 'px';
 })();
 
 // range :: int -> [int]
@@ -18,35 +18,39 @@ const styleCell = (c, left, width, top, height) => {
     c.style.top = top + '%';
     c.style.height = height + '%';
     c.setAttribute('type', 'text');
-    c.setAttribute('maxlength', '1');
+    // c.setAttribute('maxlength', '1');
 };
 
 // spillOver :: HTMLElement -> ()
-const addSpillOver = (el) => {
-    el.onkeyup = function(e) {
-        socket.emit('edit_cell', {
-            X: e.target.viewX,
-            Y: e.target.viewY,
-            value: e.target.value
-        });
-        const maxLength = 1;
-        let myLength = e.target.value.length;
-        if (myLength >= maxLength) {
-            let next = e.target;
-            while (next = next.nextElementSibling) {
-                if (next == null) 
-                    break;
-                next.focus();
-                break;
-            }
+const spillOver = function(e) {
+    let c = e.data;
+
+    if (!e.data) {
+        c = ' '
+        let prev = e.target.previousElementSibling;
+        if (prev != null) {
+            prev.focus();
+        }
+    } else {
+        let next = e.target.nextElementSibling;
+        if (next != null) {
+            next.focus();
         }
     }
+
+    e.target.value = c;
+    socket.emit('edit_cell', {
+        X: e.target.viewX,
+        Y: e.target.viewY,
+        value: c
+    });
 }
 
+// onload :: window ~> ()
 window.onload = () => {
     // window width/height in pixels
-    const wPix = window.innerWidth;
-    const hPix = window.innerHeight;
+    const wPix = grid.clientWidth;
+    const hPix = grid.clientHeight;
     // cell width and height in pixels
     const cellLPix = 25;
     // cell width/height in percent
@@ -65,7 +69,7 @@ window.onload = () => {
             styleCell(cell, left, cellWPrc, top, cellHPrc);
             cell.viewX = nW;
             cell.viewY = nH;
-            addSpillOver(cell);
+            cell.addEventListener('input', spillOver);
 
             grid.appendChild(cell);
             return cell;
@@ -75,6 +79,20 @@ window.onload = () => {
     socket = io();
 
     socket.on('connect', () => {
-        socket.emit('initialize', 'Connected!');
+        socket.emit('initialize', {
+            n_cells_w: nCellsW,
+            n_cells_h: nCellsH
+        });
+    });
+
+    socket.on('update_grid', (data) => {
+        Object.keys(data).forEach((x, i) => {
+            Object.keys(data[x]).forEach((y, j) => {
+                cells[j][i].value = data[x][y]
+            });
+        });
+    });
+    socket.on('update_cell', (data) => {
+        cells[data['Y']][data['X']].value = data['value']
     });
 };
